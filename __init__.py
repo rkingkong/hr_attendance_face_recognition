b64dd5d9ec201eb8b2b65aceed650753089b5a08
Rodolfo Kong <kongrodolfo@gmail.com>
2025-03-18 21:16:22 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,18 @@
+# -*- coding: utf-8 -*-
 from . import models
 from . import controllers
+
+# Set up dedicated logging for the face recognition module
+import logging
+
+# Create a dedicated logger with a specific name
+face_logger = logging.getLogger('odoo.addons.hr_attendance_face_recognition')
+
+# Configure the logger to include timestamps and levels
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s [Face Recognition] %(message)s')
+handler.setFormatter(formatter)
+face_logger.addHandler(handler)
+
+# Set default level to INFO, can be overridden in config
+face_logger.setLevel(logging.INFO)
