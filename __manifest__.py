7db943b8e264cb39554535d85f5f49c4c350efb7
Rodolfo Kong <kongrodolfo@gmail.com>
2025-03-19 21:57:59 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,7 @@ This module extends the HR Attendance module to add face recognition capabilitie
         'web.assets_backend': [
             'hr_attendance_face_recognition/static/src/js/kiosk_face_mode.js',
             'hr_attendance_face_recognition/static/src/css/kiosk_face_mode.css',
-           # 'hr_attendance_face_recognition/static/src/js/face_registration.js',
+            'hr_attendance_face_recognition/static/src/js/face_registration.js',
             'hr_attendance_face_recognition/static/src/css/face_registration.css',
             'hr_attendance_face_recognition/static/src/js/health_dashboard.js',
             'hr_attendance_face_recognition/static/src/css/health_dashboard.css',
